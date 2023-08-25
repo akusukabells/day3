4664227faa42971fee4d21e63ae0ea053f944139
akusukabells <wendyhalim12tkj1@gmail.com>
2023-08-26 01:36:16 +0700

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
-
+function ContainerExample() {
   return (
-    <>
-    
-    </>
-  )
+    <Container>
+      <Row>
+        <Col sm={8}>sm=8</Col>
+        <Col sm={4}>sm=4</Col>
+        <Col sm={2}>sm=2</Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default ContainerExample;
