206081476738d9c92671ae9fd4400234195cd670
Hamada702 <hamadadev.93@hotmail.com>
2023-08-26 03:19:37 +0700

    fix tscode

--- src/ListUser.jsx
+++ src/ListUser.jsx
@@ -1,46 +1,39 @@
-import React,{useState, useEffect} from "react";
-import axios from "axios"
-import { Container, Card, Row, Col, Button } from "react-bootstrap"
+import React from "react";
+import axios from "axios";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
 
+const app = new React();
 
+function ListUser() {
+  const [users, setUsers] = app.useState([]);
 
- function ListUser(){
-    
-    const [users, setUsers] = useState([]);
+  app.useEffect(() => {
+    getUsers();
+  }, []);
 
-    useEffect(()=> {
-        getUsers()
- },[]);
+  async function getUsers() {
+    const res = await axios.get("http://localhost:3002/users/");
+    setUsers(res.data);
+  }
 
-    async function getUsers(){
-        const res = await axios.get("http://localhost:3002/users/")
-        setUsers(res.data)
-    }
- 
-    return (
-        <Container fluid>
-            <Button type="submit" style={{marginBottom:"10px"}}>Create User</Button>
-            <Row>
-                {users.map(user => (
-                    
-                    <Col style={{display:"inline-block"}}>
-                    <Card style={{width:"18rem"}}>
-                        <Card.Body>
-                            <Card.Title>{user.name}</Card.Title>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                ))
-
-                }
-            </Row>
-        </Container>
-
-);
-
-
+  return (
+    <Container fluid>
+      <Button type="submit" style={{ marginBottom: "10px" }}>
+        Create User
+      </Button>
+      <Row>
+        {users.map((user) => (
+          <Col style={{ display: "inline-block" }}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{user.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-
 export default ListUser;
-
